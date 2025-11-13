b7b26a7cfdf43532159464e86fe42f05ed58751f
Luke Janssen <lukejans5@gmail.com>
2025-11-13 18:49:27 -0500

    feat: remove support for roman numerals

--- src/custom-number.sh
+++ src/custom-number.sh
@@ -10,14 +10,13 @@ format_digital="🯰🯱🯲🯳🯴🯵🯶🯷🯸🯹"
 format_fsquare="󰎡󰎤󰎧󰎪󰎭󰎱󰎳󰎶󰎹󰎼"
 format_hsquare="󰎣󰎦󰎩󰎬󰎮󰎰󰎵󰎸󰎻󰎾"
 format_dsquare="󰎢󰎥󰎨󰎫󰎲󰎯󰎴󰎷󰎺󰎽"
-format_roman=" 󱂈󱂉󱂊󱂋󱂌󱂍󱂎󱂏󱂐"
 format_super="⁰¹²³⁴⁵⁶⁷⁸⁹"
 format_sub="₀₁₂₃₄₅₆₇₈₉"
 
 ID=$1
 FORMAT=${2:-none}
 
-# Preserve leading whitespace for bash
+# preserve leading whitespace for bash
 format="$(eval echo \"\$format_"${FORMAT}"\")"
 
 if [ "${FORMAT}" = "hide" ]; then
@@ -29,12 +28,7 @@ if [ -z "${format}" ]; then
     exit 1
 fi
 
-# If format is roman numerals (-r), only handle IDs of 1 digit
-if [ "${FORMAT}" = "roman" ] && [ ${#ID} -gt 1 ]; then
-    echo -n "${ID} "
-else
-    for ((i = 0; i < ${#ID}; i++)); do
-        DIGIT=${ID:i:1}
-        echo -n "${format:DIGIT:1} "
-    done
-fi
+for ((i = 0; i < ${#ID}; i++)); do
+    DIGIT=${ID:i:1}
+    echo -n "${format:DIGIT:1} "
+done
