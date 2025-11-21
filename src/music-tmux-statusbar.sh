9af6be4e7c0eb7f39fff81dd8c09a5ad8947d63a
Luke Janssen <lukejans5@gmail.com>
2025-11-21 12:38:36 -0500

    style: reformat widgets

--- src/music-tmux-statusbar.sh
+++ src/music-tmux-statusbar.sh
@@ -5,10 +5,10 @@ ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/.."
 . "${ROOT_DIR}/lib/coreutils-compat.sh"
 
 # Check the global value
-SHOW_MUSIC=$(tmux show-option -gv @tokyo-night-tmux_show_music)
+SHOW_MUSIC=$(tmux show-option -gv @tmux-status-line_show_music)
 
 if [ "$SHOW_MUSIC" != "1" ]; then
-  exit 0
+    exit 0
 fi
 
 CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
@@ -21,117 +21,117 @@ BG_BAR="${THEME[background]}"
 TIME_COLOR="${THEME[black]}"
 
 if [[ $1 =~ ^[[:digit:]]+$ ]]; then
-  MAX_TITLE_WIDTH=$1
+    MAX_TITLE_WIDTH=$1
 else
-  MAX_TITLE_WIDTH=$(($(tmux display -p '#{window_width}' 2>/dev/null || echo 120) - 90))
+    MAX_TITLE_WIDTH=$(($(tmux display -p '#{window_width}' 2>/dev/null || echo 120) - 90))
 fi
 
 # playerctl
 if command -v playerctl >/dev/null; then
-  PLAYER_STATUS=$(playerctl -a metadata --format "{{status}};{{mpris:length}};{{position}};{{title}}" | grep -m1 "Playing")
-  STATUS="playing"
+    PLAYER_STATUS=$(playerctl -a metadata --format "{{status}};{{mpris:length}};{{position}};{{title}}" | grep -m1 "Playing")
+    STATUS="playing"
 
-  # There is no playing media, check for paused media
-  if [ -z "$PLAYER_STATUS" ]; then
-    PLAYER_STATUS=$(playerctl -a metadata --format "{{status}};{{mpris:length}};{{position}};{{title}}" | grep -m1 "Paused")
-    STATUS="paused"
-  fi
+    # There is no playing media, check for paused media
+    if [ -z "$PLAYER_STATUS" ]; then
+        PLAYER_STATUS=$(playerctl -a metadata --format "{{status}};{{mpris:length}};{{position}};{{title}}" | grep -m1 "Paused")
+        STATUS="paused"
+    fi
 
-  TITLE=$(echo "$PLAYER_STATUS" | cut -d';' --fields=4)
-  DURATION=$(echo "$PLAYER_STATUS" | cut -d';' --fields=2)
-  POSITION=$(echo "$PLAYER_STATUS" | cut -d';' --fields=3)
+    TITLE=$(echo "$PLAYER_STATUS" | cut -d';' --fields=4)
+    DURATION=$(echo "$PLAYER_STATUS" | cut -d';' --fields=2)
+    POSITION=$(echo "$PLAYER_STATUS" | cut -d';' --fields=3)
 
-  # Convert position and duration to seconds from microseconds
-  DURATION=$((DURATION / 1000000))
-  POSITION=$((POSITION / 1000000))
+    # Convert position and duration to seconds from microseconds
+    DURATION=$((DURATION / 1000000))
+    POSITION=$((POSITION / 1000000))
 
-  if [ "$DURATION" -eq 0 ]; then
-    DURATION=-1
-    POSITION=0
-  fi
+    if [ "$DURATION" -eq 0 ]; then
+        DURATION=-1
+        POSITION=0
+    fi
 
 # nowplaying-cli
 elif command -v nowplaying-cli >/dev/null; then
-  NPCLI_PROPERTIES=(title duration elapsedTime playbackRate isAlwaysLive)
-  mapfile -t NPCLI_OUTPUT < <(nowplaying-cli get "${NPCLI_PROPERTIES[@]}")
-  declare -A NPCLI_VALUES
-  for ((i = 0; i < ${#NPCLI_PROPERTIES[@]}; i++)); do
-    # Handle null values
-    [ "${NPCLI_OUTPUT[$i]}" = "null" ] && NPCLI_OUTPUT[$i]=""
-    NPCLI_VALUES[${NPCLI_PROPERTIES[$i]}]="${NPCLI_OUTPUT[$i]}"
-  done
-  if [ -n "${NPCLI_VALUES[playbackRate]}" ] && [ "${NPCLI_VALUES[playbackRate]}" -gt 0 ]; then
-    STATUS="playing"
-  else
-    STATUS="paused"
-  fi
-  TITLE="${NPCLI_VALUES[title]}"
-  if [ "${NPCLI_VALUES[isAlwaysLive]}" = "1" ]; then
-    DURATION=-1
-    POSITION=0
-  else
-    DURATION=$(printf "%.0f" "${NPCLI_VALUES[duration]}")
-    POSITION=$(printf "%.0f" "${NPCLI_VALUES[elapsedTime]}")
-
-    # fix for the bug in nowplaying-cli.
-    # See https://github.com/janoamaral/tokyo-night-tmux/issues/107#issuecomment-2576211115
-    if [[ $OSTYPE == "darwin"* ]]; then
-      if [ $STATUS == "playing" ]; then
-        echo "$POSITION" >/tmp/last_position
-      fi
-
-      if [ "$STATUS" = "paused" ]; then
-        POSITION=$(cat /tmp/last_position)
-      fi
+    NPCLI_PROPERTIES=(title duration elapsedTime playbackRate isAlwaysLive)
+    mapfile -t NPCLI_OUTPUT < <(nowplaying-cli get "${NPCLI_PROPERTIES[@]}")
+    declare -A NPCLI_VALUES
+    for ((i = 0; i < ${#NPCLI_PROPERTIES[@]}; i++)); do
+        # Handle null values
+        [ "${NPCLI_OUTPUT[$i]}" = "null" ] && NPCLI_OUTPUT[$i]=""
+        NPCLI_VALUES[${NPCLI_PROPERTIES[$i]}]="${NPCLI_OUTPUT[$i]}"
+    done
+    if [ -n "${NPCLI_VALUES[playbackRate]}" ] && [ "${NPCLI_VALUES[playbackRate]}" -gt 0 ]; then
+        STATUS="playing"
+    else
+        STATUS="paused"
     fi
+    TITLE="${NPCLI_VALUES[title]}"
+    if [ "${NPCLI_VALUES[isAlwaysLive]}" = "1" ]; then
+        DURATION=-1
+        POSITION=0
+    else
+        DURATION=$(printf "%.0f" "${NPCLI_VALUES[duration]}")
+        POSITION=$(printf "%.0f" "${NPCLI_VALUES[elapsedTime]}")
 
-  fi
+        # fix for the bug in nowplaying-cli.
+        # See https://github.com/janoamaral/tmux-status-line/issues/107#issuecomment-2576211115
+        if [[ $OSTYPE == "darwin"* ]]; then
+            if [ $STATUS == "playing" ]; then
+                echo "$POSITION" >/tmp/last_position
+            fi
+
+            if [ "$STATUS" = "paused" ]; then
+                POSITION=$(cat /tmp/last_position)
+            fi
+        fi
+
+    fi
 fi
 
 # Calculate the progress bar for sane durations
 if [ -n "$DURATION" ] && [ -n "$POSITION" ] && [ "$DURATION" -gt 0 ] && [ "$DURATION" -lt 3600 ]; then
-  TIME="[$(date -d@$POSITION -u +%M:%S) / $(date -d@$DURATION -u +%M:%S)]"
+    TIME="[$(date -d@$POSITION -u +%M:%S) / $(date -d@$DURATION -u +%M:%S)]"
 else
-  TIME="[--:--]"
+    TIME="[--:--]"
 fi
 if [ -n "$TITLE" ]; then
-  if [ "$STATUS" = "playing" ]; then
-    PLAY_STATE="░ $OUTPUT"
-  else
-    PLAY_STATE="░ 󰏤$OUTPUT"
-  fi
-  OUTPUT="$PLAY_STATE $TITLE"
+    if [ "$STATUS" = "playing" ]; then
+        PLAY_STATE="░ $OUTPUT"
+    else
+        PLAY_STATE="░ 󰏤$OUTPUT"
+    fi
+    OUTPUT="$PLAY_STATE $TITLE"
 
-  # Only show the song title if we are over $MAX_TITLE_WIDTH characters
-  if [ "${#OUTPUT}" -ge $MAX_TITLE_WIDTH ]; then
-    OUTPUT="$PLAY_STATE ${TITLE:0:$MAX_TITLE_WIDTH-1}…"
-  fi
+    # Only show the song title if we are over $MAX_TITLE_WIDTH characters
+    if [ "${#OUTPUT}" -ge $MAX_TITLE_WIDTH ]; then
+        OUTPUT="$PLAY_STATE ${TITLE:0:$MAX_TITLE_WIDTH-1}…"
+    fi
 else
-  OUTPUT=''
+    OUTPUT=''
 fi
 
 MAX_TITLE_WIDTH=25
 if [ "${#OUTPUT}" -ge $MAX_TITLE_WIDTH ]; then
-  OUTPUT="$PLAY_STATE ${TITLE:0:$MAX_TITLE_WIDTH-1}"
-  # Remove trailing spaces
-  OUTPUT="${OUTPUT%"${OUTPUT##*[![:space:]]}"}…"
+    OUTPUT="$PLAY_STATE ${TITLE:0:$MAX_TITLE_WIDTH-1}"
+    # Remove trailing spaces
+    OUTPUT="${OUTPUT%"${OUTPUT##*[![:space:]]}"}…"
 fi
 
 if [ -z "$OUTPUT" ]; then
-  echo "$OUTPUT #[fg=green,bg=default]"
+    echo "$OUTPUT #[fg=green,bg=default]"
 else
-  OUT="$OUTPUT $TIME "
-  ONLY_OUT="$OUTPUT "
-  TIME_INDEX=${#ONLY_OUT}
-  OUTPUT_LENGTH=${#OUT}
-  PERCENT=$((POSITION * 100 / DURATION))
-  PROGRESS=$((OUTPUT_LENGTH * PERCENT / 100))
-  O="$OUTPUT"
+    OUT="$OUTPUT $TIME "
+    ONLY_OUT="$OUTPUT "
+    TIME_INDEX=${#ONLY_OUT}
+    OUTPUT_LENGTH=${#OUT}
+    PERCENT=$((POSITION * 100 / DURATION))
+    PROGRESS=$((OUTPUT_LENGTH * PERCENT / 100))
+    O="$OUTPUT"
 
-  if [ $PROGRESS -le $TIME_INDEX ]; then
-    echo "#[nobold,fg=$BG_COLOR,bg=$ACCENT_COLOR]${O:0:PROGRESS}#[fg=$ACCENT_COLOR,bg=$BG_BAR]${O:PROGRESS:TIME_INDEX} #[fg=$TIME_COLOR,bg=$BG_BAR]$TIME "
-  else
-    DIFF=$((PROGRESS - TIME_INDEX))
-    echo "#[nobold,fg=$BG_COLOR,bg=$ACCENT_COLOR]${O:0:TIME_INDEX} #[fg=$BG_BAR,bg=$ACCENT_COLOR]${OUT:TIME_INDEX:DIFF}#[fg=$TIME_COLOR,bg=$BG_BAR]${OUT:PROGRESS}"
-  fi
+    if [ $PROGRESS -le $TIME_INDEX ]; then
+        echo "#[nobold,fg=$BG_COLOR,bg=$ACCENT_COLOR]${O:0:PROGRESS}#[fg=$ACCENT_COLOR,bg=$BG_BAR]${O:PROGRESS:TIME_INDEX} #[fg=$TIME_COLOR,bg=$BG_BAR]$TIME "
+    else
+        DIFF=$((PROGRESS - TIME_INDEX))
+        echo "#[nobold,fg=$BG_COLOR,bg=$ACCENT_COLOR]${O:0:TIME_INDEX} #[fg=$BG_BAR,bg=$ACCENT_COLOR]${OUT:TIME_INDEX:DIFF}#[fg=$TIME_COLOR,bg=$BG_BAR]${OUT:PROGRESS}"
+    fi
 fi
--- src/path-widget.sh
+++ src/path-widget.sh
@@ -5,13 +5,13 @@ ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/.."
 . "${ROOT_DIR}/lib/coreutils-compat.sh"
 
 # get value from tmux config
-SHOW_PATH=$(tmux show-option -gv @tokyo-night-tmux_show_path 2>/dev/null)
-PATH_FORMAT=$(tmux show-option -gv @tokyo-night-tmux_path_format 2>/dev/null) # full | relative
+SHOW_PATH=$(tmux show-option -gv @tmux-status-line_show_path 2>/dev/null)
+PATH_FORMAT=$(tmux show-option -gv @tmux-status-line_path_format 2>/dev/null) # full | relative
 RESET="#[fg=brightwhite,bg=#15161e,nobold,noitalics,nounderscore,nodim]"
 
 # check if not enabled
 if [ "${SHOW_PATH}" != "1" ]; then
-  exit 0
+    exit 0
 fi
 
 current_path="${1}"
@@ -20,7 +20,7 @@ PATH_FORMAT="${PATH_FORMAT:-$default_path_format}"
 
 # check user requested format
 if [[ ${PATH_FORMAT} == "relative" ]]; then
-  current_path="$(echo ${current_path} | sed 's#'"$HOME"'#~#g')"
+    current_path="$(echo ${current_path} | sed 's#'"$HOME"'#~#g')"
 fi
 
 echo "#[fg=blue,bg=default]░  ${RESET}#[bg=default]${current_path} "
