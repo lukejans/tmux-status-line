0833003a9caf86dfed39acbc78974a87d179a593
Luke Janssen <lukejans5@gmail.com>
2025-11-21 12:17:16 -0500

    refactor: update git status widget to be POSIX compliant
    - remove the need for gnu coreutils
    - stop making network calls for the user and instead make state changes based on the users last fetch
    - use git porcelain to parse git data more effectively

--- src/git-status.sh
+++ src/git-status.sh
@@ -1,96 +1,150 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-SHOW_NETSPEED=$(tmux show-option -gv @tokyo-night-tmux_show_git)
-if [ "$SHOW_NETSPEED" == "0" ]; then
-  exit 0
+set -u
+
+git_repo="$1"
+git_show=$(tmux show -gv @tmux-status-line_show_git 2>/dev/null)
+
+if ! cd "${git_repo}" 2>/dev/null \
+    || [ ! -d .git ] \
+    || [ "${git_show:-0}" = "0" ]; then
+    exit 0
 fi
 
-CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-source "$CURRENT_DIR/../lib/coreutils-compat.sh"
-source "$CURRENT_DIR/themes.sh"
+# Git Icons
+# ---------
+icon_change=" "
+icon_insert=" "
+icon_delete=" "
+icon_untracked=" "
+icon_move=" "
+icon_clean="󰜝 "    # 󰘬 󱓏 
+icon_dirty="󰜞 "    # 󱓎 󰜜 󰜛 󰜘
+icon_push=" "     # 󱓊 󰜞
+icon_pull=" "     # 󰓂  󰜚
+icon_diverged=" " #  󰘭 󰘬 
 
-cd "$1" || exit 1
-RESET="#[fg=${THEME[foreground]},bg=${THEME[background]},nobold,noitalics,nounderscore,nodim]"
-BRANCH=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
-STATUS=$(git status --porcelain 2>/dev/null | grep -cE "^(M| M)")
+# Git Data
+# --------
+git_data=$(git status --porcelain=v2 --branch 2>/dev/null | awk '
+    BEGIN {
+        files_changed = 0
+        files_moved = 0
+        files_untracked = 0
+        repo_ahead = 0
+        repo_behind = 0
+        git_branch = ""
+    }
 
-SYNC_MODE=0
-NEED_PUSH=0
+    /^# branch.head/ { git_branch = $3 }
+    /^# branch.ab/   {
+        repo_ahead = substr($3, 2)
+        repo_behind = substr($4, 2)
+    }
 
-if [[ ${#BRANCH} -gt 25 ]]; then
-  BRANCH="${BRANCH:0:25}…"
+    /^[12] / {
+        status = $2
+        if (status ~ /M/ || status ~ /D/) files_changed++
+        if (status ~ /R/) files_moved++
+    }
+
+    /^\?/ { files_untracked++ }
+
+    END {
+        print files_changed, files_moved, files_untracked, repo_ahead, repo_behind, git_branch
+    }
+')
+
+# shellcheck disable=SC2086
+#
+set -- $git_data
+files_changed=$1
+files_moved=$2
+files_untracked=$3
+repo_ahead=$4
+repo_behind=$5
+git_branch=$6
+set --
+
+if [ "$files_changed" -ne 0 ]; then
+    git_diff=$(git diff --shortstat 2>/dev/null)
+
+    insertions=$(
+        printf "%s\n" "$git_diff" \
+            | grep -o '[0-9]\{1,\} insertions' \
+            | cut -d' ' -f1
+    )
+    deletions=$(
+        printf "%s\n" "$git_diff" \
+            | grep -o '[0-9]\{1,\} deletions' \
+            | cut -d' ' -f1
+    )
+
+    insertions=${insertions:-0}
+    deletions=${deletions:-0}
 fi
 
-STATUS_CHANGED=""
-STATUS_INSERTIONS=""
-STATUS_DELETIONS=""
-STATUS_UNTRACKED=""
+# Branch Status
+# -------------
+[ -z "$git_branch" ] && exit 0
 
-if [[ $STATUS -ne 0 ]]; then
-  DIFF_COUNTS=($(git diff --numstat 2>/dev/null | awk 'NF==3 {changed+=1; ins+=$1; del+=$2} END {printf("%d %d %d", changed, ins, del)}'))
-  CHANGED_COUNT=${DIFF_COUNTS[0]}
-  INSERTIONS_COUNT=${DIFF_COUNTS[1]}
-  DELETIONS_COUNT=${DIFF_COUNTS[2]}
-
-  SYNC_MODE=1
+if [ "$git_branch" = "(detached)" ]; then
+    short_sha=$(git rev-parse --short HEAD)
+    git_branch="󰘦 $short_sha"
 fi
 
-UNTRACKED_COUNT="$(git ls-files --other --directory --exclude-standard | wc -l | bc)"
-
-if [[ $CHANGED_COUNT -gt 0 ]]; then
-  STATUS_CHANGED="${RESET}#[fg=${THEME[yellow]},bg=${THEME[background]},bold] ${CHANGED_COUNT} "
+if [ "${#git_branch}" -gt 25 ]; then
+    git_branch="$(echo "$git_branch" | cut -c1-25)…"
 fi
 
-if [[ $INSERTIONS_COUNT -gt 0 ]]; then
-  STATUS_INSERTIONS="${RESET}#[fg=${THEME[green]},bg=${THEME[background]},bold] ${INSERTIONS_COUNT} "
+if [ "$repo_ahead" -gt 0 ] && [ "$repo_behind" -gt 0 ]; then
+    sync_mode="diverged"
+elif [ "$repo_ahead" -gt 0 ]; then
+    sync_mode="ahead"
+elif [ "$repo_behind" -gt 0 ]; then
+    sync_mode="behind"
+elif [ "$files_changed" -gt 0 ] || [ "$files_moved" -gt 0 ] || [ "$files_untracked" -gt 0 ]; then
+    sync_mode="dirty"
+else
+    sync_mode="clean"
 fi
 
-if [[ $DELETIONS_COUNT -gt 0 ]]; then
-  STATUS_DELETIONS="${RESET}#[fg=${THEME[red]},bg=${THEME[background]},bold] ${DELETIONS_COUNT} "
+# Update Status Line
+# ------------------
+remote_status="#{@tmux-status-line_reset_style}"
+
+if [ "$sync_mode" = 'dirty' ]; then
+    remote_status="$remote_status#[bg=#{@tmux-status-line_color_bg},fg=#{@tmux-status-line_color_magenta},bold]${icon_dirty}"
+elif [ "$sync_mode" = 'ahead' ]; then
+    remote_status="$remote_status#[bg=#{@tmux-status-line_color_bg},fg=#{@tmux-status-line_color_red},bold]${icon_push}"
+elif [ "$sync_mode" = 'behind' ]; then
+    remote_status="$remote_status#[bg=#{@tmux-status-line_color_bg},fg=#{@tmux-status-line_color_red},bold]${icon_pull}"
+elif [ "$sync_mode" = 'diverged' ]; then
+    remote_status="$remote_status#[bg=#{@tmux-status-line_color_bg},fg=#{@tmux-status-line_color_red},bold]${icon_diverged}"
+else
+    remote_status="$remote_status#[bg=#{@tmux-status-line_color_bg},fg=#{@tmux-status-line_color_green},bold]${icon_clean}"
 fi
 
-if [[ $UNTRACKED_COUNT -gt 0 ]]; then
-  STATUS_UNTRACKED="${RESET}#[fg=${THEME[black]},bg=${THEME[background]},bold] ${UNTRACKED_COUNT} "
+output="$remote_status#{@tmux-status-line_reset_style}${git_branch:-} "
+
+if [ "$files_changed" -gt 0 ]; then
+    output="$output#{@tmux-status-line_reset_style}#[fg=#{@tmux-status-line_color_yellow},bg=#{@tmux-status-line_color_bg},bold]${icon_change}${files_changed} "
 fi
 
-# Determine repository sync status
-if [[ $SYNC_MODE -eq 0 ]]; then
-  NEED_PUSH=$(git log @{push}.. | wc -l | bc)
-  if [[ $NEED_PUSH -gt 0 ]]; then
-    SYNC_MODE=2
-  else
-    LAST_FETCH=$(stat -c %Y .git/FETCH_HEAD | bc)
-    NOW=$(date +%s | bc)
-
-    # if 5 minutes have passed since the last fetch
-    if [[ $((NOW - LAST_FETCH)) -gt 300 ]]; then
-      git fetch --atomic origin --negotiation-tip=HEAD
-    fi
-
-    # Check if the remote branch is ahead of the local branch
-    REMOTE_DIFF="$(git diff --numstat "${BRANCH}" "origin/${BRANCH}" 2>/dev/null)"
-    if [[ -n $REMOTE_DIFF ]]; then
-      SYNC_MODE=3
-    fi
-  fi
+if [ "${insertions:-0}" -gt 0 ]; then
+    output="$output#{@tmux-status-line_reset_style}#[fg=#{@tmux-status-line_color_green},bg=#{@tmux-status-line_color_bg},bold]${icon_insert}${insertions} "
 fi
 
-# Set the status indicator based on the sync mode
-case "$SYNC_MODE" in
-1)
-  REMOTE_STATUS="$RESET#[bg=${THEME[background]},fg=${THEME[bred]},bold]▒ 󱓎"
-  ;;
-2)
-  REMOTE_STATUS="$RESET#[bg=${THEME[background]},fg=${THEME[red]},bold]▒ 󰛃"
-  ;;
-3)
-  REMOTE_STATUS="$RESET#[bg=${THEME[background]},fg=${THEME[magenta]},bold]▒ 󰛀"
-  ;;
-*)
-  REMOTE_STATUS="$RESET#[bg=${THEME[background]},fg=${THEME[green]},bold]▒ "
-  ;;
-esac
-
-if [[ -n $BRANCH ]]; then
-  echo "$REMOTE_STATUS $RESET$BRANCH $STATUS_CHANGED$STATUS_INSERTIONS$STATUS_DELETIONS$STATUS_UNTRACKED"
+if [ "${deletions:-0}" -gt 0 ]; then
+    output="$output#{@tmux-status-line_reset_style}#[fg=#{@tmux-status-line_color_red},bg=#{@tmux-status-line_color_bg},bold]${icon_delete}${deletions} "
 fi
+
+if [ "$files_untracked" -gt 0 ]; then
+    output="$output#{@tmux-status-line_reset_style}#[fg=#{@tmux-status-line_color_brightblack},bg=#{@tmux-status-line_color_bg},bold]${icon_untracked}${files_untracked} "
+fi
+
+if [ "$files_moved" -gt 0 ]; then
+    output="$output#{@tmux-status-line_reset_style}#[fg=#{@tmux-status-line_color_white},bg=#{@tmux-status-line_color_bg},bold]${icon_move}${files_moved} "
+fi
+
+echo "$output"
