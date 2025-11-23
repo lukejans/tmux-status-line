e7b3572478f928a963543c1b2cfcdf108494e878
Luke Janssen <lukejans5@gmail.com>
2025-11-23 14:40:29 -0500

    refactor: use awk+read to parse git status and rename vars

--- src/git-widget.sh
+++ src/git-widget.sh
@@ -17,7 +17,7 @@ icon_change=" "
 icon_insert=" "
 icon_delete=" "
 icon_untracked=" "
-icon_move=" "
+icon_moved=" "
 icon_clean="󰜝 "    # 󰘬 󱓏 
 icon_dirty="󰜞 "    # 󱓎 󰜜 󰜛 󰜘
 icon_push=" "     # 󱓊 󰜞
@@ -26,47 +26,32 @@ icon_diverged=" " #  󰘭 󰘬 
 
 # Git Data
 # --------
-git_data=$(git status --porcelain=v2 --branch 2>/dev/null | awk '
-    BEGIN {
-        files_changed = 0
-        files_moved = 0
-        files_untracked = 0
-        repo_ahead = 0
-        repo_behind = 0
-        git_branch = ""
+IFS=' ' read -r branch ahead behind changes moved untracked <<EOF
+$(git status --porcelain=v2 --branch 2>/dev/null | awk '
+    /^# branch.oid/ { o_id = $3 }
+    /^# branch.head/ { head = $3 }
+    /^# branch.ab/ {
+        ahead = substr($3, 2)
+        behind = substr($4, 2)
     }
-
-    /^# branch.head/ { git_branch = $3 }
-    /^# branch.ab/   {
-        repo_ahead = substr($3, 2)
-        repo_behind = substr($4, 2)
-    }
-
     /^[12] / {
         status = $2
-        if (status ~ /M/ || status ~ /D/) files_changed++
-        if (status ~ /R/) files_moved++
+        if (status ~ /M/ || status ~ /D/) changes++
+        if (status ~ /R/) moved++
     }
-
-    /^\?/ { files_untracked++ }
-
+    /^\?/ { untracked++ }
     END {
-        print files_changed, files_moved, files_untracked, repo_ahead, repo_behind, git_branch
+        if (head == "(detached)") {
+            branch = substr(o_id, 1, 10)
+        } else {
+            branch = head
+        }
+        print branch ahead behind changes moved untracked
     }
 ')
+EOF
 
-# shellcheck disable=SC2086
-#
-set -- $git_data
-files_changed=$1
-files_moved=$2
-files_untracked=$3
-repo_ahead=$4
-repo_behind=$5
-git_branch=$6
-set --
-
-if [ "$files_changed" -ne 0 ]; then
+if [ "$changes" -ne 0 ]; then
     git_diff=$(git diff --shortstat 2>/dev/null)
 
     insertions=$(
@@ -84,26 +69,13 @@ if [ "$files_changed" -ne 0 ]; then
     deletions=${deletions:-0}
 fi
 
-# Branch Status
-# -------------
-[ -z "$git_branch" ] && exit 0
-
-if [ "$git_branch" = "(detached)" ]; then
-    short_sha=$(git rev-parse --short HEAD)
-    git_branch="󰘦 $short_sha"
-fi
-
-if [ "${#git_branch}" -gt 25 ]; then
-    git_branch="$(echo "$git_branch" | cut -c1-25)…"
-fi
-
-if [ "$repo_ahead" -gt 0 ] && [ "$repo_behind" -gt 0 ]; then
+if [ "$ahead" -gt 0 ] && [ "$behind" -gt 0 ]; then
     sync_mode="diverged"
-elif [ "$repo_ahead" -gt 0 ]; then
+elif [ "$ahead" -gt 0 ]; then
     sync_mode="ahead"
-elif [ "$repo_behind" -gt 0 ]; then
+elif [ "$behind" -gt 0 ]; then
     sync_mode="behind"
-elif [ "$files_changed" -gt 0 ] || [ "$files_moved" -gt 0 ] || [ "$files_untracked" -gt 0 ]; then
+elif [ "$changes" -gt 0 ] || [ "$moved" -gt 0 ] || [ "$untracked" -gt 0 ]; then
     sync_mode="dirty"
 else
     sync_mode="clean"
@@ -125,10 +97,10 @@ else
     remote_status="$remote_status#[bg=#{@tmux-status-line_color_bg},fg=#{@tmux-status-line_color_green},bold]${icon_clean}"
 fi
 
-output="$remote_status#{@tmux-status-line_reset_style}${git_branch:-} "
+output="$remote_status#{@tmux-status-line_reset_style}${branch:-} "
 
-if [ "$files_changed" -gt 0 ]; then
-    output="$output#{@tmux-status-line_reset_style}#[fg=#{@tmux-status-line_color_yellow},bg=#{@tmux-status-line_color_bg},bold]${icon_change}${files_changed} "
+if [ "$changes" -gt 0 ]; then
+    output="$output#{@tmux-status-line_reset_style}#[fg=#{@tmux-status-line_color_yellow},bg=#{@tmux-status-line_color_bg},bold]${icon_change}${changes} "
 fi
 
 if [ "${insertions:-0}" -gt 0 ]; then
@@ -139,12 +111,12 @@ if [ "${deletions:-0}" -gt 0 ]; then
     output="$output#{@tmux-status-line_reset_style}#[fg=#{@tmux-status-line_color_red},bg=#{@tmux-status-line_color_bg},bold]${icon_delete}${deletions} "
 fi
 
-if [ "$files_untracked" -gt 0 ]; then
-    output="$output#{@tmux-status-line_reset_style}#[fg=#{@tmux-status-line_color_brightblack},bg=#{@tmux-status-line_color_bg},bold]${icon_untracked}${files_untracked} "
+if [ "$untracked" -gt 0 ]; then
+    output="$output#{@tmux-status-line_reset_style}#[fg=#{@tmux-status-line_color_brightblack},bg=#{@tmux-status-line_color_bg},bold]${icon_untracked}${untracked} "
 fi
 
-if [ "$files_moved" -gt 0 ]; then
-    output="$output#{@tmux-status-line_reset_style}#[fg=#{@tmux-status-line_color_white},bg=#{@tmux-status-line_color_bg},bold]${icon_move}${files_moved} "
+if [ "$moved" -gt 0 ]; then
+    output="$output#{@tmux-status-line_reset_style}#[fg=#{@tmux-status-line_color_white},bg=#{@tmux-status-line_color_bg},bold]${icon_moved}${moved} "
 fi
 
 echo "$output"
